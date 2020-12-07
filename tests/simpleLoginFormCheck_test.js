6ba43bf0fcfbd251269e2a6f4babdcf4a0251171
Artem <demnart93@gmail.com>
2020-12-07 08:55:29 +0200

    Added a test to check the login form using an existing user. xpath was used as a locator.

--- tests/simpleLoginFormCheck_test.js
+++ tests/simpleLoginFormCheck_test.js
@@ -14,3 +14,12 @@ Scenario('Checking the login form when using an existing user', ({ I }) => {
     I.saveScreenshot('FirstUserSuccess.png');
     I.waitForClickable('#site-footer > div > a');
 });
+
+Scenario('Checking the login form when using the second existing user', ({ I }) => {
+    I.fillField('//*[@id="user_login"]','testuser1');
+    I.fillField('//*[@id="user_pass"]','Mae9iGei6m');
+    I.checkOption('//*[@id="rememberme"]');
+    I.click('//*[@id="wp-submit"]');
+    I.saveScreenshot('secondUserSuccess.png');
+    I.waitForClickable('#site-footer > div > a');
+});
