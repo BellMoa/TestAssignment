b5c979c6414c3f28f38b2a88f99ba56c925d5643
Artem <demnart93@gmail.com>
2020-12-07 19:47:16 +0200

    Brought the creation of an array of data to the beginning of the file.

--- tests/GPOtest_test.js
+++ tests/GPOtest_test.js
@@ -1,23 +1,30 @@
 Feature('loginAtForm');
+
+//Add Data Set
+let accounts = new DataTable(['userLogin','userPassword']);
+accounts.add(['testuser','testuser']);
+accounts.add(['testuser1','Mae9iGei6m']);
+accounts.add(['elvin','13247']);
+accounts.add(['testuser1','Teechaep8a']);
+accounts.add(['testuser1','']);
+accounts.add(['','']);
+accounts.add(['','Mae9iGei6m']);
+accounts.add(['','1324']);
+    
+
 //Before section
 Before(({ I }) => {
     //Add default page
     I.amOnPage('login-page');
-    //Add Data Set
-    let accounts = new DataTable(['userLogin','userPassword']);
-    accounts.add(['testuser','testuser']);
-    accounts.add(['testuser1','Mae9iGei6m']);
-    accounts.add(['elvin','13247']);
-    accounts.add(['testuser1','Teechaep8a']);
-    accounts.add(['testuser1','']);
-    accounts.add(['','']);
-    accounts.add(['','Mae9iGei6m']);
-    accounts.add(['','1324']);
-    const numberOfAccounts = 8;
+    
+    
 
 });
 
-Scenario('Test by GPO and Data set' ,({ I }) => {
+//Scenario that Use createde Data Set and GPO model(codeceptJS)
 
+Data(accounts).Scenario('Test By GPO model and Data Set',({ I , current ,loginFormPage }) => {
+
+    loginFormPage.loginAtForm(current.userLogin,current.userPassword);
 
 });
