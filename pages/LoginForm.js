beb03526fdc3fa7fc1da40087625d045b816aaac
Artem <demnart93@gmail.com>
2020-12-07 10:16:34 +0200

    Created an object in which declared variables (locators) to access the form fields and a method to enter the form.

--- pages/LoginForm.js
+++ pages/LoginForm.js
@@ -2,5 +2,23 @@ const { I } = inject();
 
 module.exports = {
 
-  // insert your locators and methods here
+  locators:
+  {
+    loginField: '//*[@id="user_login"]',
+    passwordField: '//*[@id="user_pass"]'
+  },
+
+  checkBox: '//*[@id="rememberme"]',
+  button: '//*[@id="wp-submit"]',
+
+
+
+  loginAtForm(login,password)
+  {
+    I.fillField(this.locators.loginField,login);
+    I.fillField(this.locators.passwordField,password);
+    I.checkOption(this.checkBox);
+    I.click(this.button);
+  }
+
 }
