464e95cf6d771f12e23a8e6240149c81fc0c04db
Artem <demnart93@gmail.com>
2020-12-07 19:37:24 +0200

    Added Data Set to Before Section

--- tests/GPOtest_test.js
+++ tests/GPOtest_test.js
@@ -1,11 +1,23 @@
 Feature('loginAtForm');
-
+//Before section
 Before(({ I }) => {
-
+    //Add default page
     I.amOnPage('login-page');
+    //Add Data Set
+    let accounts = new DataTable(['userLogin','userPassword']);
+    accounts.add(['testuser','testuser']);
+    accounts.add(['testuser1','Mae9iGei6m']);
+    accounts.add(['elvin','13247']);
+    accounts.add(['testuser1','Teechaep8a']);
+    accounts.add(['testuser1','']);
+    accounts.add(['','']);
+    accounts.add(['','Mae9iGei6m']);
+    accounts.add(['','1324']);
+    const numberOfAccounts = 8;
 
 });
 
-Scenario('LoggingAtFormByUsing GPO', ({ I }) => {
+Scenario('Test by GPO and Data set' ,({ I }) => {
+
 
 });
