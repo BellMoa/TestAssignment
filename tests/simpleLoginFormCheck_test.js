9c52febb0c7030ca0fd581c60022724cdf65c212
Artem <demnart93@gmail.com>
2020-12-07 08:53:46 +0200

    Added a test to check the login form using an existing user.

--- tests/simpleLoginFormCheck_test.js
+++ tests/simpleLoginFormCheck_test.js
@@ -6,6 +6,11 @@ Before(({ I }) => {
 
 });
 
-Scenario('test something', ({ I }) => {
-
+Scenario('Checking the login form when using an existing user', ({ I }) => {
+    I.fillField('log' , 'testuser');
+    I.fillField('pwd', 'testuser');
+    I.checkOption('rememberme');
+    I.click('wp-submit');
+    I.saveScreenshot('FirstUserSuccess.png');
+    I.waitForClickable('#site-footer > div > a');
 });
